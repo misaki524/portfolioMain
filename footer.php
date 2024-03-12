198ef4d806c472bb1df6f9ffdf3c42cbe06d3ebd
kiyo1117coda <kiyo1117coda@docomo.ne.jp>
2024-03-12 21:23:25 +0900

    字下げと背景の高さ

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <a href="#"class="p-top">TOP
-      <span class="arrow1"></span>
-      <span class="arrow2"></span>
-    </a>
-  <footer>
+  <span class="arrow1"></span>
+  <span class="arrow2"></span>
+</a>
+<footer>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/assets/stylesheet/js/slick.min.js"></script>
-  <script src="<?php echo get_template_directory_uri(); ?>/assets/stylesheet/js/main.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/stylesheet/js/main.js"></script>
   <?php wp_footer(); ?>
   </footer>
 </body>
