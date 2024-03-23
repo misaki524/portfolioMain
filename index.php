a40eb8e4b0f9910d2196eaaff2ec75749866da5d
kiyo1117coda <kiyo1117coda@docomo.ne.jp>
2024-03-23 16:43:49 +0900

    aboutページの修正

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <div class="p-page__wrap">
-    <h1 class="p-page__title">HOME</h1>
+    <h1 class="p-page__title"><a class="p-home__subText" href="<?php echo esc_url( home_url( '/' )); ?>single#linkanProfile">HOME</a></h1>
     <img class="p-page__imgItem" src="<?php echo get_template_directory_uri(); ?>/assets/images/IMG_0009.jpg" alt="">
   </div>
   <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,26 +1,30 @@
 
 <?php get_header(); ?>
 <div class="p-page__homeWrap">
+  <div class="p-page__mainGroup">
     <p class ="p-page__text">1995年5月24日生まれ<br>趣味：絵を描く。ゲームをする。読書をする。大体インドア。</p>
     <img  class="p-page__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/IMG_0007.jpg" alt="">
   </div>
-  <h1 class="p-page__title">like</h1>
-  <div class="p-page__wrap">
+  <div class="p-page__mainGroup">
     <div>
-    <p class ="p-page__text">動物：ポメラニアン・リチャードソンジリス</p>
-    <p class ="p-page__text">本：常識のない喫茶店・婚活中毒・コンビニ人間</p>
-    <p class ="p-page__text">場所：家の布団の中・自然の中・静かな場所・マッスルスナック</p>
+      <h1 class="p-page__mainTitle">like</h1>
+      <p class ="p-page__text">動物：ポメラニアン・リチャードソンジリス</p>
+      <p class ="p-page__text">本：常識のない喫茶店・婚活中毒・コンビニ人間</p>
+      <p class ="p-page__text">場所：家の布団の中・自然の中・静かな場所・マッスルスナック</p>
     </div>
     <img class="p-page__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/IMG_0008.jpg" alt="">
   </div>
-  <h1 class="p-page__title">irasuto link</h1>
-  <div class="p-page__linkGroup">
+  <div class="p-page__mainGroup">
+    <h1 class="p-page__mainTitle">irasuto link</h1>
+    <div>
     <a class="p-page__link" href="https://k-kikuchi.my.canva.site/photo-insta">
       <img class="p-page__subImg" src="<?php echo get_template_directory_uri(); ?>/assets/images/canva.jpg" alt="">
     </a>
+    </div>
+  </div>
 
 </div>
-<div class="p-wrap">
+<!-- <div class="p-wrap">
   <div class="p-wrap__mainGroup">
     <div class="p-wrap__mainItem">
       <img class="p-wrap__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/IMG_item.jpg" alt="">
@@ -192,6 +196,6 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
 <?php get_footer(); ?>
